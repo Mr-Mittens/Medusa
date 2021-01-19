5a3cb0917f43802f2e4f3200d2d9a86d1b9ebd74
Mr-Mittens <62893921+Mr-Mittens@users.noreply.github.com>
2021-01-19 23:19:17 +0530

    slight flaw covered

--- bare-setup.sh
+++ bare-setup.sh
@@ -1,7 +1,7 @@
 sudo pacman -S cmus wmname mpv chromium gvfs-mtp pcmanfm-gtk3 gvfs file-roller neofetch nitrogen ntfs-3g papirus-icon-theme picom pulseaudio pulsemixer dmenu scrot sxiv sxhkd termite ttf-nerd-fonts-symbols ttf-jetbrains-mono wget acpilight xed xorg-server xorg-xinit xorg-xrandr xorg-xsetroot --needed --noconfirm
 
-cp -r .config .xinitrc .vim .vimrc .zshrc /$HOME/ && cd dwm-6.2
+cp -r .config .xinitrc .vim .vimrc .zshrc /$HOME && cd dwm-6.2
 
-sudo rm config.h && sudo make clean install && cd ..
+sudo make clean install && cd ..
 
 cd slstatus && sudo make clean install && cd .. && cd .. && startx
